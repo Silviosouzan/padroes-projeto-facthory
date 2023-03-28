a52b4fde7749fa4d9463d630074a0915859e59fe
Silviosouzan <102241496+Silviosouzan@users.noreply.github.com>
2023-03-27 21:27:55 -0300

    Add files via upload

---
+++ padraoFactory/src/main/java/ServicoCompra.java
@@ -0,0 +1,10 @@
+public class ServicoCompra implements padroescriacao.factorymethod.IServico {
+
+    public String executar() {
+        return "Compra efetivada";
+    }
+
+    public String cancelar() {
+        return "Compra cancelada";
+    }
+}
